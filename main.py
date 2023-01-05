a1df2d51b8f09a254c415e687dbf1063fa5ee23d
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-01-05 20:56:21 +0530

    Adding CVZone Rect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import cv2
 import os
 import pickle
+import numpy as np
+import cvzone
+
+import face_recognition
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -28,9 +32,33 @@ print("Encode File Loaded")
 
 while True:
     success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
     # overlaying camera and image
     imgBackground[162:162+480, 55:55+640] = img
     imgBackground[44:44+633, 808:808 + 414] = imgModeList[1]
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("Matches", matches)
+        # print("FaceDis", faceDis)
+
+        matchIndex = np.argmin(faceDis)
+        # print("Match Index", matchIndex)
+
+        if matches[matchIndex]:
+            # print("Known Face Detected")
+            # print(studentIds[matchIndex])
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            bbox = 55+x1, 162+y1, x2 - x1, y2 - y1
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+
+
     # cv2.imshow("Webcam", img)
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
