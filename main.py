078994aff109e9222af07ba7456f067cc9c26ee2
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-01-05 13:25:21 +0530

    Encoding and Loading Encoded Files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import pickle
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -7,21 +8,29 @@ cap.set(4, 480)
 
 imgBackground = cv2.imread('Resources/background.png')
 
-#importing the mode images into a list
+# importing the mode images into a list
 folderModePath = 'Resources/Modes'
 modePathList = os.listdir(folderModePath)
 imgModeList = []
 for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
-#print(len(imgModeList))
+# print(len(imgModeList))
 
+# Load the encoding file
 
+print("Loading Encode File ... ")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded")
 
 while True:
     success, img = cap.read()
-    #overlaying camera and image
+    # overlaying camera and image
     imgBackground[162:162+480, 55:55+640] = img
     imgBackground[44:44+633, 808:808 + 414] = imgModeList[1]
-    #cv2.imshow("Webcam", img)
+    # cv2.imshow("Webcam", img)
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
