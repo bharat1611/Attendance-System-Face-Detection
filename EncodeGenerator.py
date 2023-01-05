408e2ecc6a0cde1de6d5b127ceff884e5871f16a
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-01-05 11:54:16 +0530

    Adding Images to database

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,3 +1,17 @@
 import cv2
 import face_recognition
+import pickle
+import os
 
+#importing the mode images into a list
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+imgList = []
+studentIds = []
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    #with .png
+    print(path)
+    #removing .png
+    print(os.path.splitext(path)[0])
+print(len(imgList))
